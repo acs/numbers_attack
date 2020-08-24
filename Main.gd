0784924596ebdb6f427afa87f49a7a896b54841f
Alvaro del Castillo San Felix <adelcastillo@thingso2.com>
2020-08-24 18:22:12 +0200

    Refactor levels logic

--- Main.gd
+++ Main.gd
@@ -6,29 +6,66 @@ export (PackedScene) var Mob
 var mobs = false  # mobs active or not
 var operation  # operation for the game: add, subtract, multiply, divide
 var operations  # active operations
-var score
-var total
+var score = 0
+var total = 0
 
+var max_number
 
-const MAX_NUMBER = 4
-const MAX_NUMBER_MULTIPLY = 2
-const MAX_NUMBER_DIVIDE = 2
-const LEVEL1_SCORE = 3
-const LEVEL2_SCORE = 6
-const LEVEL3_SCORE = 9
-const LEVEL4_SCORE = 10  # add mobs
+var levels  # dict with the info about the levels of the game
+
+func init_levels():
+	# key is the score needed to reach the level
+	levels = {
+		0: {
+			"numbers_speed": 3,
+			"max_number": 2,
+			"operations": [$HUD.ADD],
+			"mobs": false
+		},
+		10: {
+			"numbers_speed": 2,
+			"max_number": 3,
+			"operations": [$HUD.ADD],
+			"mobs": true
+		},
+		20: {
+			"numbers_speed": 2,
+			"max_number": 3,
+			"operations": [$HUD.ADD, $HUD.SUBTRACT],
+			"mobs": false
+		},
+		30: {
+			"numbers_speed": 3,
+			"max_number": 2,
+			"operations": [$HUD.ADD, $HUD.SUBTRACT, $HUD.MULTIPLY],
+			"mobs": false
+		},
+		40: {
+			"numbers_speed": 2,
+			"max_number": 2,
+			"operations": [$HUD.ADD, $HUD.SUBTRACT, $HUD.MULTIPLY, $HUD.DIVIDE],
+			"mobs": true
+		}
+	}
+
+func check_level():
+	if score in levels:
+		operations = levels[score]["operations"]
+		mobs = levels[score]["mobs"]
+		$NumberTimer.wait_time = levels[score]["numbers_speed"]
+		max_number = levels[score]['max_number']
 
 func _ready():
 	randomize()
+	init_levels()
+	check_level()
 
 func new_game():
-	operations = [$HUD.ADD]
-	operation = operations[0]
-	score = 0
 	total = 0
-	# $HUD.update_score(score)
+	score = 0
+	operation = operations[0]
 	$HUD.update_score(score)
-	$HUD.update_goal(operation, total, MAX_NUMBER)
+	$HUD.update_goal(operation, total, max_number)
 	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
@@ -47,16 +84,6 @@ func game_over():
 	$Player.hide()
 	$Music.stop()
 	$DeathSound.play()
-
-func check_level():
-	if score == LEVEL1_SCORE:
-		operations.append($HUD.SUBTRACT)
-	elif score == LEVEL2_SCORE:
-		operations.append($HUD.MULTIPLY)
-	elif score == LEVEL3_SCORE:
-		operations.append($HUD.DIVIDE)
-	elif score == LEVEL4_SCORE:
-		mobs = true
 
 func update_score():
 		score += 1
@@ -80,11 +107,11 @@ func check_number():
 		check_level()
 		operation = operations[randi() % operations.size()]
 		if operation == $HUD.MULTIPLY:
-			$HUD.update_goal(operation, total, MAX_NUMBER_MULTIPLY)
+			$HUD.update_goal(operation, total, max_number)
 		elif operation == $HUD.DIVIDE:
-			$HUD.update_goal(operation, total, MAX_NUMBER_DIVIDE)
+			$HUD.update_goal(operation, total, max_number)
 		else:
-			$HUD.update_goal(operation, total, MAX_NUMBER)
+			$HUD.update_goal(operation, total, max_number)
 		$Player.hit_number.queue_free()
 	
 func add_number():
@@ -103,7 +130,7 @@ func add_number():
 	# Set the velocity (speed & direction).
 	number.linear_velocity = Vector2(rand_range(number.min_speed, number.max_speed), 0)
 	number.linear_velocity = number.linear_velocity.rotated(direction)
-	number.show_number(str(round(rand_range(0, MAX_NUMBER))))
+	number.show_number(str(round(rand_range(0, max_number))))
 
 func add_mob():
 	# Create a Mob instance and add it to the scene.
